1818271d073d4b209f50fbe69da6d790d04cc3cd
Mohammed Rasif <rasifrazak123@gmail.com>
2022-02-27 16:11:25 +0530

    First react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    function App() {
+      const [toDos,setTodos] = useState([])
+      const [toDo,setTodo] = useState('')
+      return (
+        <div className="app">
+          <div className="mainHeading">
+            <h1>To-Do List</h1>
+          </div>
+          <div className="subHeading">
+            <br />
+            <h2>Whoop, it's time to getup💟   </h2>
+          </div>
+          <div className="input">
+            <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+            <i onClick={()=>setTodos([...toDos,{id:Date.now(), text:toDo,status:false}])} className="fas fa-plus"></i>
+          </div>
+          <div className="todos">
+            { toDos.map((obj)=>{
+              return(
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e)=>{
+                  console.log(e.target.checked)
+                  console.log(obj)
+                  setTodos(toDos.filter(obj2=>{
+                    if(obj2.id===obj.id){
+                      obj2.status = e.target.checked
+                    }
+                    return obj2
+                  }))
+                }} list={obj.status} type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
+            )})}
+            {toDos.map((obj)=>{
+              if(obj.status){
+                return(<h3>{obj.text}</h3>)
+              }
+              return null
+            })}
+          </div>
+        </div>
+      );
+    }
+    
+    export default App;
